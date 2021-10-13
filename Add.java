2f5cfa74557a7785b34444e22fee360665a0579e
Pratyush Dixit <83740557+pratyushdixit27@users.noreply.github.com>
2021-10-13 09:54:57 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,10 @@
+//Addition of two number
+class Add{
+public static void main(String[]args){
+int a = 10;
+int b = 20;
+int c;
+int c = a+b;
+System.out.println("Addition of both number is" +c);
+}
+}

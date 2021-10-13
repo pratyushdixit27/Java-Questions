d77fb9b9a7bac5eba49afbca606c08817388d959
Pratyush Dixit <83740557+pratyushdixit27@users.noreply.github.com>
2021-10-13 10:10:37 +0530

    Add files via upload

---
+++ Lets_Check.java
@@ -0,0 +1,11 @@
+class Lets_Check{
+int a = 10;            //Instance Variable
+static int c = 30;   //Static Variable
+public static void main(String[]args){
+int b = 20;           // Local variable. It must be initalized otherwise it will have null value
+Lets_Check c = new Lets_Check();
+System.out.println(c.a);
+System.out.println(b);
+System.out.println(d);
+}
+}

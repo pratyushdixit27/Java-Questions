145d4c1524edf3ec721fff667e5bcd3382eda121
Pratyush Dixit <83740557+pratyushdixit27@users.noreply.github.com>
2021-10-13 18:17:34 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ReverseNumber{
+public static void main(String[]args){
+Scanner sc = new Scanner(System.in){
+int num = sc.nextInt();
+int rev = 0;
+while(num!=0){
+rev = rev*10+ num%10;
+num = num/10;
+}
+System.out.println("Reverse number is: " +rev);
+}
+}
+}

188bc4e784d8475f0125ab3487583c1767090480
Pratyush Dixit <83740557+pratyushdixit27@users.noreply.github.com>
2021-10-20 09:48:59 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld{
+  
+  int a=10, b=5,c;
+  
+  
+  public void sum(){
+    c=a+b;
+    System.out.println(c);
+    
+  }
+  public static void main(String[]args){
+    HelloWorld w = new HelloWorld();
+    w.sum();
+  }
+  
+  
+}
